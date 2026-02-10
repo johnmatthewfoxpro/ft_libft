a7991ba9b0fe2e97cb7b2787842f9ab979c8a164
jfox <johnmatthewfoxpro@gmail.com>
2026-02-10 19:03:17 +0100

    Updated bzero and 3 ft_print functions that were throwing errors when compiled outside of school 42. Specifically when calling negative numbers in condtions with unsigned ints.

--- src/ft_bzero.c
+++ src/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: jfox <jfox.42angouleme@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 13:02:04 by jfox              #+#    #+#             */
-/*   Updated: 2025/12/30 18:04:22 by jfox             ###   ########.fr       */
+/*   Updated: 2026/02/10 18:59:41 by jfox             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,5 @@
 
 void	ft_bzero(void *str, size_t n)
 {
-	unsigned char	*ptr;
-
-	ptr = (unsigned char *)str;
-	ptr = ft_memset(str, 0, n);
+	ft_memset(str, 0, n);
 }
